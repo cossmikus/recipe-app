e2743ce9d64b4a17a837213941090aee57cae27f
Kassymkhan Bolat <cossmikus@gmail.com>
2024-09-19 16:30:54 +0500

    v9

--- app/app/settings.py
+++ app/app/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'PORT': os.getenv('DB_PORT'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'PASSWORD': os.getenv('DB_PASS'),
     }
 }
 
