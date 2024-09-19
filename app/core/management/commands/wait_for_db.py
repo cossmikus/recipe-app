ae5767a2894cc73562be2d8bf45712cc097566ca
Kassymkhan Bolat <cossmikus@gmail.com>
2024-09-19 16:08:24 +0500

    v5

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -16,12 +16,19 @@ class Command(BaseCommand):
         """Entrypoint for command."""
         self.stdout.write('Waiting for database...')
         db_up = False
-        while db_up is False:
+        retries = 0
+        max_retries = 30  # Retry for 30 seconds
+        while not db_up and retries < max_retries:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
+                retries += 1
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
-        self.stdout.write(self.style.SUCCESS('Database available!'))
+        if db_up:
+            self.stdout.write(self.style.SUCCESS('Database available!'))
+        else:
+            self.stdout.write(self.style.ERROR('Database unavailable after waiting!'))
+            raise OperationalError('Database unavailable after waiting!')
